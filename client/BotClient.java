b100b1c935c4b00ca23ffad0587bec677901b990
Vladyslav94 <grigoren_vlad2009@yahoo.com>
2020-03-04 16:02:07 +0200

    Let`s create an opportunity for client to talk with bot

--- client/BotClient.java
+++ client/BotClient.java
@@ -1,4 +1,29 @@
 package com.javarush.task.task30.task3008.client;
 
-public class BotClient {
+public class BotClient extends Client{
+
+    public static void main(String[] args) {
+        BotClient botClient = new BotClient();
+        botClient.run();
+    }
+
+    public class BotSocketThread extends SocketThread{
+
+    }
+
+    @Override
+    protected String getUserName() {
+        int num = (int)(Math.random()*100);
+        return "date_bot_" + num;
+    }
+
+    @Override
+    protected boolean shouldSendTextFromConsole() {
+        return false;
+    }
+
+    @Override
+    protected SocketThread getSocketThread() {
+        return new BotSocketThread();
+    }
 }
